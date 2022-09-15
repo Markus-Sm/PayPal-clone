951c7549329d0d48838fc68582b07391b4744fa7
Marek Samofał <96687413+Markus-Sm@users.noreply.github.com>
2022-09-15 11:35:00 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,89 @@
+// Pages
+document.querySelectorAll('.logo').forEach(logo => {
+	logo.addEventListener('click', () => {
+		document.querySelector('.front-page').style.display = 'block'
+		document.querySelector('.login-page').style.display = 'none'
+		document.querySelector('.signup-page').style.display = 'none'
+	})
+})
+
+document.querySelectorAll('.login').forEach(loginBtn => {
+	loginBtn.addEventListener('click', () => {
+		document.querySelector('.front-page').style.display = 'none'
+		document.querySelector('.login-page').style.display = 'block'
+		document.querySelector('.signup-page').style.display = 'none'
+	})
+})
+
+document.querySelectorAll('.signup').forEach(signupBtn => {
+	signupBtn.addEventListener('click', () => {
+		document.querySelector('.front-page').style.display = 'none'
+		document.querySelector('.login-page').style.display = 'none'
+		document.querySelector('.signup-page').style.display = 'flex'
+	})
+})
+// End of Pages
+
+/* Navigation */
+const dropdownItems = document.querySelectorAll('.dropdown-hover')
+
+if(window.innerWidth < 1000){
+	const menuIcon = document.querySelector('.menu')
+    const navbar = document.querySelector('.navbar')
+    
+    menuIcon.addEventListener('click', () => {
+        navbar.classList.toggle('change')
+
+        if(!navbar.classList.contains('change')) {
+            document.querySelectorAll('.nav-dropdown').forEach(dropdown => {
+                dropdown.style.left = '-20rem'
+            })
+        }
+    })
+
+    document.querySelectorAll('.show-dropdown').forEach(link => {
+        link.addEventListener('click', () => {
+            link.nextElementSibling.style.left = '0'
+        })
+    })
+
+    document.querySelectorAll('.dropdown-heading-link').forEach(headingLink => {
+        headingLink.addEventListener('click', () => {
+            headingLink.parentElement.parentElement.style.left = '-20rem'
+        })
+    })
+
+}else{
+	dropdownItems.forEach(dropdownItems => {
+		dropdownItems.addEventListener('mouseover', () => {
+			dropdownItems.lastElementChild.style.cssText = 'opacity: 1; visibility: visible'
+			document.querySelector('.navbar-wrapper').style.background = 'linear-gradient(to right, #066399, #2f8fdf, #066399)'
+			dropdownItems.firstElementChild.firstElementChild.style.transform = "rotate(180deg)"
+		})
+	
+		dropdownItems.addEventListener('mouseout', () => {
+			dropdownItems.lastElementChild.style.cssText = 'opacity: 0; visibility: hidden'
+			document.querySelector('.navbar-wrapper').style.background = 'none'
+			dropdownItems.firstElementChild.firstElementChild.style.transform = "rotate(0)"
+		})
+	})
+	
+	const navDropdownItem = document.querySelectorAll('.nav-dropdown-item')
+	
+	navDropdownItem.forEach(navDropdownItem => {
+		navDropdownItem.addEventListener('mouseover', () => {
+			navDropdownItem.lastElementChild.style.cssText = 'text-decoration: underline'
+			navDropdownItem.firstElementChild.style.cssText = 'text-decoration: underline'
+		})
+	
+		navDropdownItem.addEventListener('mouseout', () => {
+			navDropdownItem.lastElementChild.style.cssText = 'text-decoration: none'
+			navDropdownItem.firstElementChild.style.cssText = 'text-decoration: none'
+		})
+	})
+}
+
+window.addEventListener('resize', ()=> {
+	window.location.reload()
+})
+/* End of Navigation */
